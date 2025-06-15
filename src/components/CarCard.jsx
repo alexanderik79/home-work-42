cc975fab86d054effaed3274078d766716e19b3c
alexanderik79 <alexanderik79@gmail.com>
2025-06-15 17:29:32 +0200

    css

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -6,9 +6,9 @@ function CarCard({ car }) {
       <h3>{car.name}</h3>
       <p>{car.description}</p>
       <p>Price: ${car.price.toLocaleString()}</p>
-      <p>Year: {car.year}</p>
-      <p>Mileage: {car.mileage.toLocaleString()} miles</p>
-      <p>Color: {car.color}</p>
+      {/* <p>Year: {car.year}</p> */}
+      {/* <p>Mileage: {car.mileage.toLocaleString()} miles</p> */}
+      {/* <p>Color: {car.color}</p> */}
       {car.photoList && car.photoList[0] && (
         <img src={car.photoList[0]} alt={car.name} style={{ maxWidth: '200px' }} />
       )}
